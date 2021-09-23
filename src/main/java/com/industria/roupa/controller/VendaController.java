26a907e6e282b7e6e68d73790b85b159ccf3418d
Douglas Y. Yabusame <dyabusame@itelinfo.com>
2021-09-23 15:57:40 -0400

    controller venda

---
+++ src/main/java/com/industria/roupa/controller/VendaController.java
@@ -0,0 +1,63 @@
+package com.industria.roupa.controller;
+
+import com.industria.roupa.Mensagem;
+import com.industria.roupa.biz.VendaBiz;
+import com.industria.roupa.entities.Venda;
+import com.industria.roupa.repositories.VendaRepository;
+import com.industria.roupa.repositories.LojaRepository;
+import com.industria.roupa.repositories.RoupaRepository;
+import com.industria.roupa.repositories.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import javax.validation.ConstraintViolationException;
+
+@RestController
+@RequestMapping("venda")
+public class VendaController {
+	
+	@Autowired
+	private VendaRepository vendaRepository;
+
+    @Autowired
+    private LojaRepository lojaRepository;
+
+    @Autowired
+    private RoupaRepository roupaRepository;
+    
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    @GetMapping
+    @RequestMapping("listar")
+    public List<Venda> Get() {
+        return vendaRepository.findAll();
+    }
+
+
+    @PostMapping
+    @RequestMapping("incluir")
+    public Mensagem add(@RequestBody Venda venda) {
+        VendaBiz vendaBiz = new VendaBiz(lojaRepository, roupaRepository, funcionarioRepository);
+        venda.setIdVenda(0);
+
+        try {
+
+            if (vendaBiz.Validade(venda)) {
+                this.vendaRepository.save(venda);
+                this.vendaRepository.flush();
+            } else {
+                return vendaBiz.msg;
+            }
+        }catch (ConstraintViolationException e) {
+            e.getConstraintViolations().forEach(v -> vendaBiz.msg.mensagens.add(v.getMessage()));
+            return vendaBiz.msg;
+        }
+
+        return vendaBiz.msg;
+
+
+    }
+}
