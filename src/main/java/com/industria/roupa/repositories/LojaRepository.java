8ce99b3575d0f2e45168b9ffe216b249d3ed0301
Kevin Olentino <32975170+KevinOlentino@users.noreply.github.com>
2021-09-23 15:13:24 -0400

    Criando LojaRepositorio

---
+++ src/main/java/com/industria/roupa/repositories/LojaRepository.java
@@ -0,0 +1,7 @@
+package com.industria.roupa.repositories;
+
+import com.industria.roupa.entities.Loja;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LojaRepository extends JpaRepository<Loja,Integer> {
+}
