88e693807c5fe7c740c59c5e020ed7a66ba0f8a1
Kevin Olentino <32975170+KevinOlentino@users.noreply.github.com>
2021-09-23 14:51:07 -0400

    Create Setor.java

---
+++ src/main/java/com/industria/roupa/entities/Setor.java
@@ -0,0 +1,36 @@
+package com.industria.roupa.entities;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Pattern;
+
+@Entity
+@Table(name="Setor")
+public class Setor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idsetor")
+    private int idSetor;
+
+    @Column(name = "nome")
+    @Max(50)
+    @Pattern(regexp = "[A-z ]{5,50}")
+    private String nome;
+
+    public int getIdSetor() {
+        return idSetor;
+    }
+
+    public void setIdSetor(int idSetor) {
+        this.idSetor = idSetor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
