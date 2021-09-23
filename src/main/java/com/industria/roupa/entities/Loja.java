d9e08dd3c42fd84b59cddce9923f9af5b99238f7
Marciel S. Soares <msoares@itelinfo.com>
2021-09-23 15:21:05 -0400

    Criacao das entidades

--- src/main/java/com/industria/roupa/entities/Loja.java
+++ src/main/java/com/industria/roupa/entities/Loja.java
@@ -26,7 +26,7 @@ public class Loja {
     @Column(name = "credito")
     @Max(50)
     @Pattern(regexp = "[A-z ]{5,50}")
-    private String Credito;
+    private Double Credito;
 
     public int getIdloja() {
 		return idloja;
@@ -44,11 +44,11 @@ public class Loja {
 		Nome = nome;
 	}
 
-	public String getCredito() {
+	public Double getCredito() {
 		return Credito;
 	}
 
-	public void setCredito(String credito) {
+	public void setCredito(Double credito) {
 		Credito = credito;
 	}
 
