9fee779f6c0a66f94ed74933c7acdf24ba640800
Kevin Olentino <32975170+KevinOlentino@users.noreply.github.com>
2021-09-23 15:01:05 -0400

    Adicionando Roupa,Setor e Tecido Repositorio

---
+++ src/main/java/com/industria/roupa/repositories/RoupaRepository.java
@@ -0,0 +1,7 @@
+package com.industria.roupa.repositories;
+
+import com.industria.roupa.entities.Roupa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoupaRepository extends JpaRepository<Roupa,Integer> {
+}
---
+++ src/main/java/com/industria/roupa/repositories/TecidoRepository.java
@@ -0,0 +1,7 @@
+package com.industria.roupa.repositories;
+
+import com.industria.roupa.entities.Tecido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TecidoRepository extends JpaRepository<Tecido,Integer> {
+}
