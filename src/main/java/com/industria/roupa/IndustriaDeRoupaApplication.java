cecbda495f23c84d7ef881abebf00548dac8b394
Kevin Olentino <32975170+KevinOlentino@users.noreply.github.com>
2021-09-23 14:19:29 -0400

    First Commit

---
+++ src/main/java/com/industria/roupa/IndustriaDeRoupaApplication.java
@@ -0,0 +1,13 @@
+package com.industria.roupa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustriaDeRoupaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustriaDeRoupaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/industria/roupa/IndustriaDeRoupaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.industria.roupa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustriaDeRoupaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
