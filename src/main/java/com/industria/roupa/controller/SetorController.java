6f4061b88d3a79eec784d24c7cd06317f7dad557
Andre L. Proenca <aproenca@itelinfo.com>
2021-09-23 17:06:19 -0400

    SetorController

---
+++ src/main/java/com/industria/roupa/controller/SetorController.java
@@ -0,0 +1,55 @@
+package com.industria.roupa.controller;
+
+import java.util.List;
+import javax.validation.ConstraintViolationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.industria.roupa.Mensagem;
+import com.industria.roupa.biz.SetorBiz;
+import com.industria.roupa.entities.Setor;
+import com.industria.roupa.repositories.SetorRepository;
+
+
+
+@RestController
+@RequestMapping("setor")
+public class SetorController {
+	
+	@Autowired
+	private SetorRepository setorRepository;
+	
+	@GetMapping
+    @RequestMapping("listar")
+    public List<Setor> Get() {
+        return setorRepository.findAll();
+    }
+
+
+    @PostMapping
+    @RequestMapping("incluir")
+    public Mensagem add(@RequestBody Setor setor) {
+        SetorBiz setorBiz = new SetorBiz(setorRepository);
+        setor.setIdSetor(0);
+
+        try {
+
+            if (setorBiz.Validade(setor)) {
+                this.setorRepository.save(setor);
+                this.setorRepository.flush();
+            } else {
+                return setorBiz.msg;
+            }
+        }catch (ConstraintViolationException e) {
+            e.getConstraintViolations().forEach(v -> setorBiz.msg.mensagens.add(v.getMessage()));
+            return setorBiz.msg;
+        }
+
+        return setorBiz.msg;
+    }
+	
+
+}
