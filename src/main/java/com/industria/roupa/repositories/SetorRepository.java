07ce6be566157877f07426844e69024dc732759d
Kevin Olentino <32975170+KevinOlentino@users.noreply.github.com>
2021-09-23 15:02:47 -0400

    Adicionando DataAnnotations nos repositorios

--- src/main/java/com/industria/roupa/repositories/SetorRepository.java
+++ src/main/java/com/industria/roupa/repositories/SetorRepository.java
@@ -2,6 +2,8 @@ package com.industria.roupa.repositories;
 
 import com.industria.roupa.entities.Setor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SetorRepository extends JpaRepository<Setor,Integer> {
 }
