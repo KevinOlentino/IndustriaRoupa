6cab8eb05e429794556591b2d48b0a8b254a69b5
Marciel S. Soares <msoares@itelinfo.com>
2021-09-23 17:31:40 -0400

    Criacao do pacote biz

---
+++ src/main/java/com/industria/roupa/controller/FuncionarioController.java
@@ -0,0 +1,70 @@
+package com.industria.roupa.controller;
+
+import java.text.ParseException;
+import java.util.List;
+
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.industria.roupa.Mensagem;
+import com.industria.roupa.biz.FuncionarioBiz;
+import com.industria.roupa.entities.Funcionario;
+import com.industria.roupa.repositories.FuncaoRepository;
+import com.industria.roupa.repositories.FuncionarioRepository;
+import com.industria.roupa.repositories.SetorRepository;
+
+@RestController
+@RequestMapping("Funcionario")
+public class FuncionarioController {
+	
+	
+			
+		@Autowired
+		private FuncionarioRepository funcionarioRepository;
+
+	    @Autowired
+	    private FuncaoRepository funcaoRepository;
+	    
+	    @Autowired
+	    private SetorRepository setorRepository;
+	      
+	    @GetMapping
+	    @RequestMapping("listar")
+	    public List<Funcionario> Get() {
+	        return funcionarioRepository.findAll();
+	    }
+
+
+	    @PostMapping
+	    @RequestMapping("incluir")
+	    public Mensagem add(@RequestBody Funcionario funcionario) throws ParseException {
+	    	FuncionarioBiz funcionarioBiz = new FuncionarioBiz( setorRepository,  funcaoRepository);
+	        funcionario.setId(0);
+
+	        try {
+
+	            if (funcionarioBiz.Validade(funcionario)) {
+	                this.funcionarioRepository.save(funcionario);
+	                this.funcionarioRepository.flush();
+	            } else {
+	                return funcionarioBiz.msg;
+	            }
+	        }catch (ConstraintViolationException e) {
+	            e.getConstraintViolations().forEach(v -> funcionarioBiz.msg.mensagens.add(v.getMessage()));
+	            return funcionarioBiz.msg;
+	        }
+
+	        return funcionarioBiz.msg;
+
+
+	    }
+	}
+
+
+
---
+++ src/main/java/com/industria/roupa/controller/LojaController.java
@@ -0,0 +1,62 @@
+package com.industria.roupa.controller;
+
+import java.util.List;
+
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.industria.roupa.Mensagem;
+import com.industria.roupa.biz.LojaBiz;
+import com.industria.roupa.entities.Loja;
+import com.industria.roupa.repositories.LojaRepository;
+
+
+@RestController
+@RequestMapping("Loja")
+public class LojaController {
+	
+	
+			
+		@Autowired
+		private LojaRepository lojaRepository;
+
+		@GetMapping
+	    @RequestMapping("listar")
+	    public List<Loja> Get() {
+	        return lojaRepository.findAll();
+	    }
+
+
+	    @PostMapping
+	    @RequestMapping("incluir")
+	    public Mensagem add(@RequestBody Loja loja) {
+	    	LojaBiz lojaBiz = new LojaBiz(lojaRepository);
+	    	loja.setIdloja(0);
+
+	        try {
+
+	            if (lojaBiz.Validade(loja)) {
+	                this.lojaRepository.save(loja);
+	                this.lojaRepository.flush();
+	            } else {
+	                return lojaBiz.msg;
+	            }
+	        }catch (ConstraintViolationException e) {
+	            e.getConstraintViolations().forEach(v -> lojaBiz.msg.mensagens.add(v.getMessage()));
+	            return lojaBiz.msg;
+	        }
+
+	        return lojaBiz.msg;
+
+
+	    }
+	}
+
+
+
