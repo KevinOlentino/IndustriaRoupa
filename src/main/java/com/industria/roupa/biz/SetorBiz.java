a9db1059dbce3a633d59f96c651ab90b0e8a592e
Kevin Olentino <32975170+KevinOlentino@users.noreply.github.com>
2021-09-23 15:49:13 -0400

    Adicionando SetorBiz e Atualizando SetorRepositorio

---
+++ src/main/java/com/industria/roupa/biz/SetorBiz.java
@@ -0,0 +1,23 @@
+package com.industria.roupa.biz;
+
+import com.industria.roupa.Mensagem;
+import com.industria.roupa.entities.Setor;
+import com.industria.roupa.repositories.SetorRepository;
+
+public class SetorBiz {
+    public Mensagem msg;
+
+    public SetorRepository setorRepository;
+
+    public boolean Validade(Setor setor) {
+
+        boolean result = true;
+        
+        if (!setorRepository.findByName(setor.getNome()).isEmpty()){
+            msg.mensagens.add("Esse setor já foi adicionado.");
+            result = false;
+        }
+
+        return result;
+    }
+}
