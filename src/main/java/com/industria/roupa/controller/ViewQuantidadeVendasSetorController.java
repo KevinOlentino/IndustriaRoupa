0dae5e04c78035017c5bade78af707b5513d4129
Kevin Olentino <32975170+KevinOlentino@users.noreply.github.com>
2021-09-29 16:57:01 -0400

    Update ViewQuantidadeVendasSetorController.java

--- src/main/java/com/industria/roupa/controller/ViewQuantidadeVendasSetorController.java
+++ src/main/java/com/industria/roupa/controller/ViewQuantidadeVendasSetorController.java
@@ -3,6 +3,7 @@ package com.industria.roupa.controller;
 import com.industria.roupa.repositories.ViewQuantidadeVendasSetorRepository;
 import com.industria.roupa.views.ViewQuantidadeVendasSetor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("viewquantidadevendassetor")
+@CrossOrigin
 public class ViewQuantidadeVendasSetorController {
     @Autowired
     private ViewQuantidadeVendasSetorRepository viewQuantidadeVendasSetorRepository;
